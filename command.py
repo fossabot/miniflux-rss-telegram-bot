2ee317e058a18994ba7af86eda87e6eeac9a5b37
blue-bird1 <linlanxi7552659@gmail.com>
2018-10-30 23:10:14 +0800

    fucntion self explain

--- command.py
+++ command.py
@@ -2,11 +2,78 @@
 import io
 import json
 import telegram 
+from inspect import *
 from client import new_client
-from constant import NO_BIND_MSG, DISCOVER_USAGE_MSG, GET_ENTRIES_USAGE_MSG
+from constant import NO_BIND_MSG, CREATE_OK_MSG, ADD_FEED_OK_MSG, ID_NO_INT_MSG
 from telegram import InputFile
+from client import new_client
+from module import DBSession
+from module.user import User
+from config import admin_client
 from miniflux import ClientError
+
+
+def start(bot, update):
+    """
+    欢迎使用rss机器人,使用/help获取更多帮助
+    """
+    bot.send_message(chat_id=update.message.chat_id, text=getdoc(globals()[getframeinfo(currentframe()).function]))
+
+def bind(bot, update, args):
+    """
+    usage: /bind username password
+    """
+    if len(args) != 2:
+       bot.send_message(chat_id=update.message.chat_id, text=getdoc(globals()[getframeinfo(currentframe()).function]))
+       return
+    session = DBSession()
+    user = User(id=update.message.chat_id, username=args[0], password=args[1]) 
+    session.merge(user)
+    session.commit()
+    session.close()
+    bot.send_message(chat_id=update.message.chat_id, text=BIND_OK_MSG)
+
+def new_user(bot, update, args):
+    """
+    usage: /new_user username password
+    """
+    if len(args) != 2:
+       bot.send_message(chat_id=update.message.chat_id, text=getdoc(globals()[getframeinfo(currentframe()).function]))
+       return
+    try:
+        admin_client.create_user(args[0],args[1], False)    
+    except ClientError as e: # pylint: disable=invalid-name
+        bot.send_message(chat_id=update.message.chat_id, text=e.get_error_reason())
+        return 
+    bot.send_message(chat_id=update.message.chat_id, text=CREATE_OK_MSG)
+    
+def add_feed(bot, update, args):
+    """
+    usage: /addfeed url category_id
+    """
+    if len(args) != 2: 
+       bot.send_message(chat_id=update.message.chat_id, text=ADD_FEED_USAGE_MSG)
+       return
+    if not args[1].isdecimal():
+       bot.send_message(chat_id=update.message.chat_id, text=ID_NO_INT_MSG)
+       return
+    try:
+        client = new_client(update.message.chat_id)
+    except UserNotBindError as e: # pylint: disable=invalid-name
+        bot.send_message(chat_id=update.message.chat_id, text=NO_BIND_MSG)
+        return    
+    try:
+        client.create_feed(args[0], int(args[1]))
+    except ClientError as e: # pylint: disable=invalid-name
+        bot.send_message(chat_id=update.message.chat_id, text=e.get_error_reason())
+        return
+    bot.send_message(chat_id=update.message.chat_id, text=ADD_FEED_OK_MSG)
+
+
 def export(bot, update):
+    """
+    export user feed to opml file
+    """
     try:
         client = new_client(update.message.chat_id)
     except UserNotBindError:
@@ -22,8 +89,11 @@ def export(bot, update):
     opml_file.close()
 
 def discover(bot, update, args):
+    """    
+    usage: /discover url
+    """
     if len(args) != 1: 
-       bot.send_message(chat_id=update.message.chat_id, text=DISCOVER_USAGE_MSG)
+       bot.send_message(chat_id=update.message.chat_id, text=getdoc(globals()[getframeinfo(currentframe()).function]))
        return
     try:
         client = new_client(update.message.chat_id)
@@ -38,8 +108,11 @@ def discover(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id,text="发现成功 订阅地址{}".format(ret[0]['url'])) 
 
 def get_entries(bot, update, args):
+    """
+    usage: /get_entries num
+    """
     if len(args) != 1: 
-       bot.send_message(chat_id=update.message.chat_id, text=GET_ENTRIES_USAGE_MSG)
+       bot.send_message(chat_id=update.message.chat_id, text=getdoc(globals()[getframeinfo(currentframe()).function]))
        return
     try:
         client = new_client(update.message.chat_id)
