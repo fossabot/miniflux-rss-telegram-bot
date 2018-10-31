b7cd1d1c0f7706ecd6d2b12b727c8b6fc4912eea
blue-bird1 <linlanxi7552659@gmail.com>
2018-10-31 14:49:53 +0800

    test

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,14 +1,21 @@
 #coding:utf-8
 # pylint:disable=all
 import sys
+import os
 sys.path.append('..')
 from tgintegration import BotIntegrationClient
 from constant import *
 from command import *
 
+botname = os.environ.get('botname')
+app_id = os.environ.get('app_id')
+app_hash = os.environ.get('app_hash')
+_
 client = BotIntegrationClient(
-    bot_under_test='@bluebirdrss_bot',
+    bot_under_test=botname,
     session_name = 'account'
+    api_id=app_id,
+    api_hash =api_hash,
 )
 
 
