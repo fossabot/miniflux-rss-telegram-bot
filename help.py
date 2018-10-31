63daa30554adafdc7e9a25bb90fe390910368e3b
blue-bird1 <linlanxi7552659@gmail.com>
2018-10-31 09:53:24 +0800

    add help

---
+++ help.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+import inspect
+
+def get_module_function__doc(module):
+    module = __import__(module)
+    name_func_tuples = inspect.getmembers(module, inspect.isfunction)
+    name_func_tuples = [t[1] for t in name_func_tuples if inspect.getmodule(t[1]) == module]
+    tmp = ''
+    for _ in name_func_tuples:
+        tmp = tmp+_.__doc__
+    return tmp
+
+def help(bot, update): 
+    bot.send_message(chat_id=update.message.chat_id, text=get_module_function__doc('command'))
