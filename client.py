9bc1c7bf871d75fe44a13e25d7b0e57544d60da0
blue-bird1 <linlanxi7552659@gmail.com>
2018-11-01 20:42:21 +0800

    add test

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
-#coding:utf-8
+# coding:utf-8
 from module import DBSession
 from module.user import User
 from error import UserNotBindError
 from miniflux import Client
 from config import SERBER_ADDR
 
-def new_client(user_id: str) -> Client:
-    session = DBSession()
+def new_client(user_id: str, session=DBSession) -> Client:
+    session = session()
     user = session.query(User).filter(User.id == user_id).first()
     session.close()
-    if user.id == '':
-       raise UserNotBindError 
+    if user is None:
+       raise UserNotBindError
     return Client(SERBER_ADDR,username=user.username, password=user.password)
-    
-    
